b0b002e999face41f2d8cbf79ada0498deb381df
len23 <lenin.montalvo.23@hotmail.com>
2018-02-12 21:16:36 -0500

    Agregado de vista AcercaDe

---
+++ EPNMuseum/MuseoVirtual/Controllers/AcercaDeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MuseoVirtual.Controllers
+{
+    public class AcercaDeController : Controller
+    {
+        // GET: AcercaDe
+        public ActionResult Mostrar_AcercaDe()
+        {
+            return View();
+        }
+    }
+}
